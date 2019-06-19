4a42c7cfdf23c3ab2babad18488fcbbaf0eb8ee8
Aderbal <aderbal@MacBook-Air-de-Bruno.local>
2019-06-19 17:32:45 -0300

    refactor(app): Lesson 11

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,7 @@
+const validator = require( 'validator' );
 const notes = require( './notes.js' );
 
 console.log( notes( ) );
+
+console.log( validator.isEmail( 'myname@example.com' ) );
+console.log( validator.isURL( 'https://mead.io' ) );
