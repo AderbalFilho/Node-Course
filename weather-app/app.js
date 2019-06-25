cb60ba299d82a4119215eebcc38cecd5053bc5fc
AderbalFilho <aderbalcrato@yahoo.com.br>
2019-06-25 14:01:20 -0300

    refactor(app): Lesson 33

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,15 @@
 const request = require( 'request' );
 
-// I'm not going to make an account!
+// I'm not going to make any account!
 const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-122.4233?lang=pt';
+const geocodeUrl = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-122.4233?lang=pt';
 
 request( { url: url, json: true }, ( error, response ) => {
     console.log( response.body.daily.data[0].summary + 'It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance fo rain.' );
+});
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+request( { url: geocodeUrl, json: true }, ( error, response ) => {
+    console.log( 'Latitude: ' + response.body.features[0].center[1] + '. Longitude: ' + response.body.features[0].center[0] + '.' );
 });
