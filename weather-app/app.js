ad6cb6608bd921b7ffaf388a13cb8ae98fd7f16e
AderbalFilho <aderbalcrato@yahoo.com.br>
2019-07-01 17:27:26 -0300

    refactor(app): Lesson 38

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,12 +3,23 @@ const forecast = require( './utils/forecast' );
 
 // I'm not going to make any account!
 
-geocode( 'Boston', ( error, data ) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+const address = process.argv[2];
 
-forecast( -75.7088, 44.1545, ( error, data ) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+if ( !address ) {
+    console.log( 'Please provide an address' );
+} else {
+    geocode( address, ( error, data ) => {
+        if ( error ) {
+            console.log('Error', error);
+        }
+        if ( data ) {
+            forecast( data.latitude, data.longitude, ( error, forecastData ) => {
+                if ( error ) {
+                    return console.log('Error', error);
+                }
+                console.log(data.location);
+                console.log(forecastData);
+            });
+        }
+    });
+}
