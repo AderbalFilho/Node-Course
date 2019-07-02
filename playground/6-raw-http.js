2759c434800f57a62cec2c2d16af3e9f736a4f2c
AderbalFilho <aderbalcrato@yahoo.com.br>
2019-07-02 09:41:36 -0300

    refactor(app): Lesson 41

---
+++ playground/6-raw-http.js
@@ -0,0 +1,23 @@
+const https = require( 'https' );
+
+const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/40,-75?lang=pt';
+
+const request = https.request(url, ( response ) => {
+    let data = '';
+
+    response.on( 'data', ( chunk ) => {
+        data = data + chunk.toString();
+        console.log( chunk );
+    });
+
+    response.on( 'end', ( ) => {
+        const body = JSON.parse( data );
+        console.log( body );
+    });
+})
+
+request.on( 'error', ( error ) => {
+    console.log('An error', error);
+});
+
+request.end( );
