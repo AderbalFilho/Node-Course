6759b4a15778d9d0132c6488e9c0e789621ce34f
AderbalFilho <aderbalcrato@yahoo.com.br>
2019-07-02 10:44:03 -0300

    refactor(app): Lesson 44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,35 @@ const express = require( 'express' );
 const app = express( );
 
 app.get( '', ( req, res ) => {
-    res.send( 'Hello express!' );
+    res.send( '<h1>Weather</h1>' );
 });
 
 app.get( '/help', ( req, res ) => {
-    res.send( 'Help page' );
+    res.send([
+        {
+            name: 'Andrew',
+            age: 27
+        },
+        {
+            name: 'Sarah',
+            age: 25
+        }
+    ]);
 });
 
 app.get( '/about', ( req, res ) => {
-    res.send( 'About page' );
+    res.send( '<h1>About page</h1>' );
 });
 
 app.get( '/weather', ( req, res ) => {
-    res.send( 'Show weather' );
+    res.send({
+        forecast: 'New York. It is currently 37 degrees out. There is a 47% chance of rain.',
+        location: 'New York',
+        latitude: 47,
+        longitude: -23,
+        temperature: 37,
+        chanceRain: 47
+    });
 });
 
 app.listen( 3000, ( ) => {
